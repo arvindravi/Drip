46e20d121c605846dce06ddba7917973dc04fa19
Arvind Ravi <arvind.ravi@icloud.com>
2021-11-05 13:35:05 +0530

    Initial Commit

---
+++ Drip/DripApp.swift
@@ -0,0 +1,17 @@
+//
+//  DripApp.swift
+//  Drip
+//
+//  Created by Arvind Ravi on 05/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct DripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
